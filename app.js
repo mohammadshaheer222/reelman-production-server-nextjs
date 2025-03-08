2d32faacf724ea3161b5da5c2379411a2f4fa5e9
mohammad shaheer <shaheercse2022@gmail.com>
2025-03-08 20:00:57 +0530

    updated the cors origin

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ app.use("/uploads", express.static("uploads"))
 app.use(express.urlencoded({ extended: true }))
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: [
+            "http://localhost:3000", 
+            "https://reelman-production-nextjs-zgaf.vercel.app"
+        ],
         credentials: true,
     })
 )
