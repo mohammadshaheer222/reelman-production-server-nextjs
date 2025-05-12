1799e04abe513d57a260369a9d3d33e2c416bcc3
mohammad shaheer <shaheercse2022@gmail.com>
2025-05-12 23:44:44 +0530

    Added morgan package

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express")
 const cookieParser = require("cookie-parser")
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const helmet = require("helmet")
+const morgan = require("morgan")
 
 const ErrorHandler = require("./middlewares/Error")
 const notFound = require("./middlewares/not-found")
@@ -20,6 +22,8 @@ app.use(
     })
 )
 
+app.use(morgan("common"))
+app.use(helmet());
 app.use(express.json({ limit: '50mb' }))
 app.use(cookieParser())
 
