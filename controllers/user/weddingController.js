752202b9af8f78c3073fb7625361fbb5e439639a
mohammad shaheer <shaheercse2022@gmail.com>
2025-05-17 21:27:25 +0530

    added get single wedding api

--- controllers/user/weddingController.js
+++ controllers/user/weddingController.js
@@ -1,6 +1,7 @@
 const CatchAsyncErrors = require("../../middlewares/CatchAsyncErrors")
 const WeddingModel = require("../../models/weddingModel")
 const ErrorHandler = require("../../utils/ErrorHandler.js")
+const mongoose = require("mongoose")
 
 const getWedding = CatchAsyncErrors(async (req, res, next) => {
     try {
@@ -26,13 +27,22 @@ const getSingleCard = CatchAsyncErrors(async (req, res, next) => {
 
 const getSingleWedding = CatchAsyncErrors(async (req, res, next) => {
     try {
-        const { id: weddingId } = req.params
-        const weddingDetails = await WeddingModel.findOne({ _id: weddingId })
-        if (!weddingDetails) {
-            return next(new ErrorHandler("No details with this id", 400))
+        const categoryTitle = req.params.id; 
+        const categoryDoc = await mongoose.model('category').findOne({ category: categoryTitle });
+
+        if (!categoryDoc) {
+            return next(new ErrorHandler(`Category "${categoryTitle}" not found`, 404));
         }
-        res.status(200).json({ success: true, weddingDetails })
+
+        const weddings = await WeddingModel.find({ category: categoryDoc._id })
+            .populate('category');
+
+        if (weddings.length === 0) {
+            return next(new ErrorHandler(`No weddings found for category "${categoryTitle}"`, 404));
+        }
+        res.status(200).json({ success: true, weddings })
     } catch (error) {
+        console.log("error")
         return next(new ErrorHandler(error.message, 500))
     }
 })
